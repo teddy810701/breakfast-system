bbcb7818066c776115e9f8f44f483cb3df849229
yuxian <s0928758338@gmail.com>
2026-04-07 10:05:10 +0800

    fix build success version

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAfBj728Hs928rZByNebgCkcJoU_MNxFIs",
@@ -13,5 +13,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
